0f1eb2a6c4074d8b0f798bd998101f7a3888a7cf
Maty Masaryk <xmasarykm1@stuba.sk>
2023-05-12 17:33:30 +0200

    style: add some comments

--- z05.py
+++ z05.py
@@ -11,9 +11,11 @@ import numpy as np
 import time
 from math import ceil
 
-IMAGE_DIR_PATH = "images"       # Input images should be stored in this dir
-OUTPUT_DIR_PATH = "output"      # Converted images will be stored to this dir
-MAX_IMAGES = 100                # Maximum number of converted images
+IMAGE_DIR_PATH = "images"  # Input images should be stored in this dir
+OUTPUT_DIR_PATH = "output"  # Converted images will be stored to this dir
+MAX_IMAGES = 100  # Maximum number of converted images
+TBP_HEIGHT = 10  # Threads per block vertically
+TBP_WIDTH = 10  # Threads per block horizontally
 
 
 def grayscale_cpu(in_data: list, out_data: list, height: int, width: int):
@@ -28,9 +30,11 @@ def grayscale_cpu(in_data: list, out_data: list, height: int, width: int):
     """
     for i in range(height):
         for j in range(width):
+            # Calculate grayscale
             new_color = 0.299 * in_data[i][j][0] + \
                         0.587 * in_data[i][j][1] + \
                         0.114 * in_data[i][j][2]
+            # Save to output
             out_data[i][j] = [new_color, new_color, new_color]
 
 
@@ -47,15 +51,17 @@ def grayscale_gpu(in_data: np.array, out_data: list, height: int, width: int):
     """
     row, col = cuda.grid(2)
     if row < height and col < width:
+        # Calculate grayscale
         new_color = 0.299 * in_data[row][col][0] + \
                     0.587 * in_data[row][col][1] + \
                     0.114 * in_data[row][col][2]
+        # Save to output
         out_data[row][col] = [new_color, new_color, new_color]
 
 
 def transform_to_grayscale(input_pixels: list, use_gpu: bool):
     """
-    Transform pixels to greyscale, using either CPU or GPU.
+    Transform pixels to grayscale, using either CPU or GPU.
 
     Calculate execution time of the process.
 
@@ -71,11 +77,16 @@ def transform_to_grayscale(input_pixels: list, use_gpu: bool):
     height = len(input_pixels)
     width = len(input_pixels[0])
     output_pixels = input_pixels.copy()
+    # Run using GPU
     if use_gpu:
-        tpb = (10, 10)
+        # Set threads per block
+        tpb = (TBP_HEIGHT, TBP_WIDTH)
+        # Set blocks per grid
         bpg = (ceil(height / tpb[0]), ceil(width / tpb[1]))
+        # Run kernel
         grayscale_gpu[tpb, bpg](np.array(input_pixels),
                                 output_pixels, height, width)
+    # Run using CPU
     else:
         grayscale_cpu(input_pixels, output_pixels, height, width)
     end_time = time.time()
@@ -94,9 +105,12 @@ def transform_images(use_gpu: bool):
     """
     exec_times = []
     for i, filename in enumerate(glob.iglob(f'{IMAGE_DIR_PATH}/*')):
+        # Read pixels
         pixels = plt.imread(filename)
+        # Get transformed pixels
         new_pixels, runtime = transform_to_grayscale(pixels, use_gpu)
         exec_times.append(runtime)
+        # Save to output file
         plt.imsave(filename.replace(IMAGE_DIR_PATH, OUTPUT_DIR_PATH),
                    new_pixels, format="jpg")
         if i > MAX_IMAGES - 2:
